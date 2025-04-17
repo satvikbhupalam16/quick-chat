b63e8887dd1554064e3390a110dbcd9533a4460a
Satvik.bhupalam <satvik.bhupalam16@gmail.com>
2025-04-17 13:35:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname)));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('set name', (data) => {
+    console.log('User name set:', data.name);
+    socket.username = data.name;
+    socket.emit('name set', { name: data.name });
+  });
+
+  socket.on('chat message', (data) => {
+    data.status = 'sent';
+    io.emit('chat message', data);
+  });
+
+  socket.on('message delivered', (data) => {
+    data.status = 'delivered';
+    io.emit('update status', data);
+  });
+
+  socket.on('message seen', (data) => {
+    data.status = 'seen';
+    io.emit('update status', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
